9e698c1f68c04e62eb2740a08600e132cfd67972
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-05-25 20:05:56 -0300

    Corrigindo problema no código que exibe (Conta bloqueada) mesmo quando a senha está correta na última tentativa

--- src/VerificarConta.java
+++ src/VerificarConta.java
@@ -9,24 +9,32 @@ public class VerificarConta {
         System.out.println(" - Faça login na sua conta bancaria - ");
         System.out.print("Informe seu nome: ");
         String login = scanner.nextLine();
-        System.out.print("Digite sua senha: ");
-        int senhaLogin = scanner.nextInt();
-        System.out.println("*****************************************");
 
         String tipoConta = "Corrente";
         double saldo = 2500;
         int senha = 1234;
         int tentativasLogin = 3;
+        boolean loginBemSucedido = false;
 
         for (int i = 1; i <= tentativasLogin; i++ ){
+            System.out.print("Digite sua senha: ");
+            int senhaLogin = scanner.nextInt();
+
             if(senhaLogin == senha){
-                System.out.println("Bem vindo " + login + "!");
+                loginBemSucedido = true;
+                break;
+            }else {
+                if (i < tentativasLogin){
+                    System.out.println("Senha incorreta, tente novamente.");
+                }
+            }
 
-                System.out.println("*****************************************");
+        }
+        if(loginBemSucedido){
+            System.out.println("Bem vindo " + login + "!");
+            System.out.println("*****************************************");
 
-                String detalheDaConta;
-
-                detalheDaConta = String.format("""
+            String detalheDaConta = String.format("""
                 Dados iniciais do cliente:
                 
                 Nome:               %s
@@ -34,33 +42,12 @@ public class VerificarConta {
                 Saldo inicial:      R$ %.2f
                 """, login,tipoConta,saldo);
 
-                System.out.println(detalheDaConta);
-                System.out.println("*****************************************");
+            System.out.println(detalheDaConta);
+            System.out.println("*****************************************");
 
-                break;
-            }else if (senhaLogin != senha){
-                System.out.print("Senha incorreta, tente novamente: ");
-                senhaLogin = scanner.nextInt();
-            }if (i == tentativasLogin){
-                System.out.println("Conta bloqueada! Favor, entrar em contato com o suporte.");
-            }
+        } else {
+            System.out.println("Conta bloqueada! Favor, entrar em contato com o suporte.");
         }
-
-      /*  String detalheDaConta;
-
-        detalheDaConta = String.format("""
-                Nome:               %s
-                Tipo de conta:      %s
-                Saldo inicial:      R$ %f
-                """, login,tipoConta,saldo);
-
-        System.out.println(detalheDaConta);
-        System.out.println("************************************");
-
-*/
-
-
-
         scanner.close();
     }
 }

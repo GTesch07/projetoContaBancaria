b697f2b80fb60dcca8f5e5df76a9407fea0ee116
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-05-30 18:28:07 -0300

    Adicionando modificadores de acesso e métodos Getters e Setters, juntamente de um novo diretório/pacote onde contem todos os modelos.

--- src/Conta.java
+++ src/Conta.java
@@ -1,37 +1,46 @@
-import java.sql.SQLOutput;
+import tesch.guilherme.contabancaria.modelos.InformacoesConta;
+
 import java.util.Scanner;
 
-public class VerificarConta {
+public class Conta {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-
+        InformacoesConta info = new InformacoesConta();
 
         System.out.println("*****************************************");
         System.out.println(" - Faça login na sua conta bancaria - ");
         System.out.print("Informe seu nome: ");
         String login = scanner.nextLine();
 
-        String tipoConta = "Corrente";
-        double saldo = 2500;
-        int senha = 1234;
-        int tentativasLogin = 3;
-        boolean loginBemSucedido = false;
+        //       String tipoConta = "Corrente";
+        //       double saldo = 2500;
+        //       int senha = 1234;
+//        int tentativasLogin = 3;
+        //       boolean loginBemSucedido = false;
 
-        for (int i = 1; i <= tentativasLogin; i++ ){
+        info.setTipoConta("Corrente");
+        info.setSaldo(2500);
+        info.setSenha(1234);
+        info.setTentativasLogin(3);
+        info.setLoginBemSucedido(false);
+
+
+
+        for (int i = 1; i <= info.getTentativasLogin(); i++ ){
             System.out.print("Digite sua senha: ");
             int senhaLogin = scanner.nextInt();
 
-            if(senhaLogin == senha){
-                loginBemSucedido = true;
+            if(senhaLogin == info.getSenha()){
+                info.setLoginBemSucedido(true);
                 break;
             }else {
-                if (i < tentativasLogin){
+                if (i < info.getTentativasLogin()){
                     System.out.println("Senha incorreta, tente novamente.");
                 }
             }
 
         }
-        if(loginBemSucedido){
+        if(info.isLoginBemSucedido()){
             String bemVindo =String.format("""
                     
                     *****************************************
@@ -47,13 +56,13 @@ public class VerificarConta {
                 Nome:               %s
                 Tipo de conta:      %s
                 Saldo inicial:      R$ %.2f
-                """, login,tipoConta,saldo);
+                """, login,info.getTipoConta(),info.getSaldo());
 
             System.out.println(detalheDaConta);
             System.out.println("*****************************************");
 
             while(true){
-                String operacao = String.format("""
+                String operacao = """
                     
                     Operações
                     
@@ -62,7 +71,7 @@ public class VerificarConta {
                     3- Transferir valor
                     4- Sair
                     
-                    """);
+                    """;
 
                 System.out.println(operacao);
                 System.out.print("Digite a opção desejada: ");
@@ -70,22 +79,22 @@ public class VerificarConta {
 
                 switch (opcaoOperacao){
                     case 1:
-                        System.out.println("O saldo atual é R$ " + saldo+"\n");
+                        System.out.println("O saldo atual é R$ " + info.getSaldo()+"\n");
                         break;
                     case 2:
                         System.out.print("Informe o valor a receber: ");
                         double valorRecebido = scanner.nextDouble();
-                        saldo += valorRecebido;
-                        System.out.println("Saldo atualizado R$ "+ saldo);
+                        info.setSaldo(info.getSaldo() + valorRecebido);
+                        System.out.println("Saldo atualizado R$ "+ info.getSaldo());
                         break;
                     case 3:
                         System.out.print("Informe o valor a transferir: ");
                         double valorTransferencia = scanner.nextDouble();
-                        if (valorTransferencia > saldo){
+                        if (valorTransferencia > info.getSaldo()){
                             System.out.println("Saldo insuficiente!");
                         }else {
-                            saldo -= valorTransferencia;
-                            System.out.println("Saldo atualizado R$ "+ saldo);
+                            info.setSaldo(info.getSaldo()-valorTransferencia);
+                            System.out.println("Saldo atualizado R$ "+ info.getSaldo());
                         }
                         break;
                     case 4:
---
+++ src/tesch/guilherme/contabancaria/modelos/InformacoesConta.java
@@ -0,0 +1,50 @@
+package tesch.guilherme.contabancaria.modelos;
+
+public class InformacoesConta {
+    private String tipoConta;
+    private double saldo;
+    private int senha;
+    private int tentativasLogin;
+    private boolean loginBemSucedido;
+
+    public String getTipoConta(){
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta){
+        this.tipoConta = tipoConta;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+
+    }
+
+    public int getSenha(){
+        return senha;
+    }
+
+    public void setSenha(int senha){
+        this.senha = senha;
+    }
+
+    public int getTentativasLogin(){
+        return tentativasLogin;
+    }
+
+    public void setTentativasLogin(int tentativasLogin){
+        this.tentativasLogin = tentativasLogin;
+    }
+
+    public boolean isLoginBemSucedido(){
+        return loginBemSucedido;
+    }
+
+    public void setLoginBemSucedido(boolean loginBemSucedido){
+        this.loginBemSucedido = loginBemSucedido;
+    }
+}
